00b1d5dac415932969f855e99031f0bfd21c92c4
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2025-12-03 10:35:37 +0530

    DAY 6 — Next.js Fundamentals (Start strong)

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata = {
+  title: "About Us - Next.js",
+  description: "About page SEO",
+} as Metadata;
+
+export default function About() {
+  return <h1>About Page</h1>;
+}
